c3696e9d7bffc51a93ddc7a731b0281c1153d1c7
MarcoNachenius <marconachenius@gmail.com>
2025-10-03 21:52:11 +0200

    update

---
+++ bash/2_download_oracle_19c.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ORACLE_DOWNLOAD_URL="https://download.oracle.com/otn/linux/oracle19c/190000/oracle-database-ee-19c-1.0-1.x86_64.rpm"
+ORACLE_PROFILE_NAME="marconachenius@gmail.com"
+ORACLE_PROFILE_PASSWORD="BePoes1992@"
+
+# Download package into tmp/
+wget --user="$ORACLE_PROFILE_NAME" --password="$ORACLE_PROFILE_PASSWORD" \
+     --no-check-certificate \
+     "$ORACLE_DOWNLOAD_URL" -O /tmp/oracle-database-ee-19c-1.0-1.x86_64.rpm
--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-bash bash/0_install_odb_19c.sh
+bash bash/0_preinstall_odb_19c.sh
 bash bash/1_configure_odb.sh
